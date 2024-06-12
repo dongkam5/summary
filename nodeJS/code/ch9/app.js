088345ed09215e5ccbb6885bd232985ba27e6afd
dongkam <84716824+dongkam5@users.noreply.github.com>
2024-06-12 22:47:24 +0900

    using passport to make login function

--- nodeJS/code/ch9/app.js
+++ nodeJS/code/ch9/app.js
@@ -5,14 +5,17 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const passport = require('passport');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
 const {sequelize} = require('./models');
 const cookieParser = require('cookie-parser');
+const passportConfig = require('./passport')
 
 const app = express();
-
+passportConfig();
 app.set('port',process.env.PORT||8001);
 app.set('view engine','html');
 nunjucks.configure('views',{
@@ -41,9 +44,10 @@ app.use(session({
         secure:false,
     },
 }));
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/',pageRouter);
-
+app.use('/auth',authRouter);
 app.use((req,res,next)=>{
 const error= new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
 error.status=404;
---
+++ nodeJS/code/ch9/routes/auth.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
+const User = require('../models/user');
+
+
+const router = express.Router();
+
+router.post('/join',isNotLoggedIn,async(req,res,next)=>{
+    const{email,nick,password} = req.body;
+    try{
+        const exUser = await User.findOne({where:{email}});
+        if(exUser){
+            return res.redirect('/join?error=exist');
+        }
+        const hash = await bcrypt.hash(password,12);
+        await User.create({
+            email,
+            nick,
+            password:hash,
+        provider:'local'}
+        );
+        return res.redirect('/');
+    }
+    catch (error){
+        console.error(error);
+        return next(error);
+    }
+});
+
+router.post('/login',isNotLoggedIn,(req,res,next)=>{
+    passport.authenticate('local',(authError,user,info)=>{
+        if(authError){
+            console.error(authError);
+            return next(authError);
+        }
+        if (!user){
+            return res.redirect(`/?loginError=${info.message}`);
+        }
+        return req.login(user,(loginError)=>{
+            if(loginError){
+                console.error(loginError);
+                return next(loginError);
+            }
+            return res.redirect('/');
+        });
+    })(req,res,next);
+});
+
+router.get('/logout', isLoggedIn, (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            console.error(err);
+            return next(err);
+        }
+        req.session.destroy((err) => {
+            if (err) {
+                console.error(err);
+                return next(err);
+            }
+            console.log('로그아웃 잘됨!');
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/kakao',passport.authenticate('kakao'));
+
+router.get('/kakao/callback',passport.authenticate('kakao',{
+    failureRedirect:'/',
+}),(req,res)=>{
+    res.redirect('/');
+});
+
+module.exports=router;
